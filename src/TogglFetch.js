b9ae554e38ce8f04c4b4ed15844ea4573d9232ef
Eran Geva <eran.geva@gmail.com>
2022-09-24 09:36:41 +0300

    getparams

--- src/TogglFetch.js
+++ src/TogglFetch.js
@@ -1,30 +1,25 @@
 export class TogglFetch {
     constructor(togglApiKey) {
         this.auth = btoa(togglApiKey + `:api_token`);
-    }
-
-    async fetchTimes(start, end) {
-        const geturl = `https://api.track.toggl.com/api/v9/me/time_entries?start_date=${start}&end_date=${end}`;
-        const resp = await fetch(geturl, {
+        this.getparams = {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
                 "Authorization": `Basic ${this.auth}`
-            },
-        });
+            }
+        };
+    }
+
+    async fetchTimes(start, end) {
+        const geturl = `https://api.track.toggl.com/api/v9/me/time_entries?start_date=${start}&end_date=${end}`;
+        const resp = await fetch(geturl, this.getparams);
         const json = await resp.json();
         return json;
     }
 
     async fetchProjects() {
         const geturl = `https://api.track.toggl.com/api/v9/me/projects`;
-        const resp = await fetch(geturl, {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Basic ${this.auth}`
-            },
-        });
+        const resp = await fetch(geturl, this.getparams);
         const json = await resp.json();
         return json;
     }
