17bea90719eed0ba13dd4b95b2da18605739f026
Eran Geva <eran.geva@gmail.com>
2022-09-24 09:48:36 +0300

    xref times and projects

--- src/TogglFetch.js
+++ src/TogglFetch.js
@@ -1,3 +1,5 @@
+import moment from "moment";
+import 'moment-duration-format';
 export class TogglFetch {
     constructor(togglApiKey) {
         this.auth = btoa(togglApiKey + `:api_token`);
@@ -22,5 +24,32 @@ export class TogglFetch {
         const resp = await fetch(geturl, this.getparams);
         const json = await resp.json();
         return json;
+    }
+
+    async getTimes(start, end) {
+        const times = await this.fetchTimes(start, end);
+        // console.log(Object.keys(json[0]).join(','));
+        // console.log(Object.values(json[0]).join(','));
+        const projects = await this.fetchProjects();
+        times.forEach(row => {
+            row.projectName = projects.find(proj => proj.id === row.project_id).name;
+            row.start_time = moment(row.start).format('YYYY.MM.DD HH:mm:ss');
+            row.stop_time = moment(row.stop).format('YYYY.MM.DD HH:mm:ss');
+            row.duration_time = moment.utc(moment(row.stop).diff(moment(row.start))).format('HH:mm:ss');
+        })
+        return { times, projects };
+    }
+
+    static jsonToCsv(items, fields) {
+        const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
+        let header = Object.keys(items[0]);
+        if (fields) {
+            header = header.filter(x => fields.includes(x));
+        }
+        const csv = [
+            header.join(','), // header row first
+            ...items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
+        ].join('\n');
+        return csv;
     }
 }
--- src/TogglShow.js
+++ src/TogglShow.js
@@ -19,48 +19,20 @@ export const TogglShow = ({ togglApiKey }) => {
         return { start, end };
     }
 
-    const jsonToCsv = (items, fields) => {
-        const replacer = (key, value) => value === null ? '' : value; // specify how you want to handle null values here
-        let header = Object.keys(items[0]);
-        if (fields) {
-            header = header.filter(x => fields.includes(x));
-        }
-        const csv = [
-            header.join(','), // header row first
-            ...items.map(row => header.map(fieldName => JSON.stringify(row[fieldName], replacer)).join(','))
-        ].join('\n');
-        return csv;
-    }
-
     const run = async () => {
         const { start, end } = timesMonth();
-        const json = await toggl.fetchTimes(start, end);
-        console.log(Object.keys(json[0]).join(','));
-        console.log(Object.values(json[0]).join(','));
-        const projectsJson = await toggl.fetchProjects();
-        json.forEach(row => {
-            row.projectName = projectsJson.find(proj => proj.id === row.project_id).name;
-            row.start_time = moment(row.start).format('YYYY.MM.DD HH:mm:ss');
-            row.stop_time = moment(row.stop).format('YYYY.MM.DD HH:mm:ss');
-            row.duration_time = moment.utc(moment(row.stop).diff(moment(row.start))).format('HH:mm:ss');
-        })
-        setProjectNames(projectsJson);
-        setTimeEntries(json);
-        console.log(jsonToCsv(json, false));
+        const {times, projects} = await toggl.getTimes(start, end);
+        setProjectNames(projects);
+        setTimeEntries(times);
     }
 
-    // let header = timeEntries.length ? Object.keys(timeEntries[0]) : [];
     const header = ['description', 'start_time', 'stop_time', 'duration_time'];
-    // if (fields) {
-    //     header = header.filter(x => fields.includes(x));
-    // }
-
-    const projects = Array.from(new Set(timeEntries.map(x => x.project_id)));
+    const shownProjects = Array.from(new Set(timeEntries.map(x => x.project_id)));
 
     return (
         <div>
             <button onClick={() => run()}>Get Time Entries!</button>
-            {projects.map(project => {
+            {shownProjects.map(project => {
                 const times = timeEntries.filter(x => x.project_id === project);
                 const allDiffs = times.map(row => moment(row.stop).diff(moment(row.start)));
                 const totalTime = allDiffs.reduce((pv, cv) => pv + cv, 0);
