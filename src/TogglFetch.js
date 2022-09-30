94521f237f8862d45481c80f3c469d3640daf48e
Eran Geva <eran.geva@gmail.com>
2022-09-30 14:01:20 +0300

    cleanup

--- src/TogglFetch.js
+++ src/TogglFetch.js
@@ -4,44 +4,30 @@ import 'moment-duration-format';
 export class TogglFetch {
     constructor(togglApiKey) {
         this.auth = btoa(togglApiKey + `:api_token`);
-        this.instance = axios.create({
+        this.togglAxios = axios.create({
             baseURL: 'https://api.track.toggl.com/api/v9',
             headers: {
                 'Content-Type': 'application/json',
                 "Authorization": `Basic ${this.auth}`
             },
         });
-        // this.getparams = {
-        //     method: "GET",
-        //     headers: {
-        //         "Content-Type": "application/json",
-        //     }
-        // };
     }
 
     async fetchTimes(start, end) {
-        const resp = await this.instance.get('me/time_entries', { params: {
+        const resp = await this.togglAxios.get('me/time_entries', { params: {
             start_date: start,
             end_date: end,
         } });
-        // const geturl = `https://api.track.toggl.com/api/v9/me/time_entries?start_date=${start}&end_date=${end}`;
-        // const resp = await axios.get(geturl, {}, this.getparams.headers);
-        const json = await resp.data;
-        return json;
+        return resp.data;
     }
 
     async fetchProjects() {
-        const resp = await this.instance.get('me/projects');
-        // const geturl = `https://api.track.toggl.com/api/v9/me/projects`;
-        // const resp = await axios.get(geturl, {}, this.getparams.headers);
-        const json = await resp.data;
-        return json;
+        const resp = await this.togglAxios.get('me/projects');
+        return resp.data;
     }
 
     async getTimes(start, end) {
         const times = await this.fetchTimes(start, end);
-        // console.log(Object.keys(json[0]).join(','));
-        // console.log(Object.values(json[0]).join(','));
         const projects = await this.fetchProjects();
         times.forEach(row => {
             row.projectName = projects.find(proj => proj.id === row.project_id).name;
