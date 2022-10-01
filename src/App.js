ec99bab36c487b6b70b5f4c427569d1878d0262c
Eran Geva <eran.geva@gmail.com>
2022-10-01 11:59:32 +0300

    footer text

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import useLocalStorage from 'use-local-storage';
 import { TogglShow } from './TogglShow.js';
 import { DateRange } from './DateRange.js';
 import { useState } from 'react';
-import { Box, createTheme, CssBaseline, ThemeProvider, Typography } from '@mui/material';
+import { Box, createTheme, CssBaseline, Link, ThemeProvider, Typography } from '@mui/material';
 
 const darkTheme = createTheme({
     palette: {
@@ -27,6 +27,10 @@ function App() {
                 <DateRange range={range} setRange={setRange}></DateRange>
                 <TogglShow togglApiKey={togglApiKey} range={range}></TogglShow>
             </Box>
+            <Typography variant='body2' position={'absolute'} bottom={0} align='center' left={0} right={0}>
+                This app is not Affiliated with Toggl. Your data is used only locally.<br/>
+                <Link href="https://github.com/erasta/toggl-report">Source code on github   </Link>
+            </Typography>
         </ThemeProvider>
     );
 }
